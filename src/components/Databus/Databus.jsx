bfa045df79f6b991868e8e3b3d1159965dece2b0
nguyetpham09 <nguyetpham969@gmail.com>
2021-08-15 22:06:10 +0700

    fix UI for bus manage

--- src/components/Databus/Databus.jsx
+++ src/components/Databus/Databus.jsx
@@ -3,7 +3,10 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ConfirmDialog from "../../shared/partials/ConfirmDialog";
 import Loading from "../../shared/partials/Loading";
-import SearchAdmin from "../Search_admin/SearchAdmin";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarker, faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+
 import "./Databus.scss";
 Databus.propTypes = {};
 
@@ -11,6 +14,15 @@ function Databus(props) {
   const [buses, setBuses] = useState();
   const [isDeleted, setIsDeleted] = useState(false);
   const removeId = useRef();
+  const [garages, setGarages] = useState([]);
+
+  useEffect(() => {
+    fetch("https://qlbvxk.herokuapp.com/api/garages")
+      .then((res) => res.json())
+      .then((result) => {
+        setGarages(result);
+      });
+  });
 
   function handleDelete(id) {
     setIsDeleted(false);
@@ -52,6 +64,41 @@ function Databus(props) {
         setBuses(result);
       });
   }, []);
+
+  function handleBack() {
+    window.location.href = "/bus";
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    let maNx = document.getElementById("nhaXe").value;
+
+    if (!(maNx)) {
+      axios
+        .get("https://qlbvxk.herokuapp.com/api/buses")
+        .then((response) => {
+          console.log(response.data);
+          setBuses(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      axios
+        .get(
+          `https://qlbvxk.herokuapp.com/api/buses/garage?garageid=${maNx}`
+        )
+        .then((response) => {
+          console.log(response.data);
+          setBuses(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+
   return (
     <div>
       <div className="searchForm">
@@ -60,11 +107,36 @@ function Databus(props) {
           <div className="my-row">
             <div className="table-list my-table-list">
               <div>
-                <SearchAdmin
-                  className="edit-search"
-                  onSubmit={handleFiltersChange}
-                />
+                <form className="my-search-form-bus">
+                  <div class="input-group">
+                    <div class="input-group-prepend ">
+                      <span class="input-group-text">
+                        Nhà xe
+                      </span>
+                    </div>
+                    <select
+                      type="text"
+                      id="nhaXe"
+                      class="form-control my-search-form__input"
+                    >
+                      {garages.map((garage) => {
+                        return (
+                          <option key={garage.maNhaXe} value={garage.maNhaXe}>
+                            {garage.tenNhaXe}
+                          </option>
+                        );
+                      })}
+                    </select>
+
+
+                    <button className="btn btn-primary" onClick={handleSearch}>
+                      Tìm xe
+                    </button>
+                    <button className="btn btn-primary" onClick={handleBack}>Tất cả</button>
+                  </div>
+                </form>
               </div>
+
               <div>
                 <button className="btn btn-primary button addbusbutton myeditbutton">
                   <Link to="/bus/add" className="link-add-button my-button">
@@ -97,7 +169,7 @@ function Databus(props) {
                           <tr>
                             <td data-column="numberplate">{bus.bienSoXe}</td>
 
-                            <td data-column="nhaxe">{bus.nhaXe}</td>
+                            <td data-column="nhaxe">{bus.tenNhaXe}</td>
 
                             <td data-column="tennv">{bus.tenNv}</td>
 
